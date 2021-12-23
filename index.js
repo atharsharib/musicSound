289f20812a27734c1917f8477bfd5ad001365a15
atharsharib <91632406+atharsharib@users.noreply.github.com>
2021-12-24 02:01:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,31 +15,31 @@ addEventListener("keypress" , function (event){
 
 function makeSound(key){
  switch (key) {
-  case "w": var audiow= new Audio("sounds/crash.mp3");
+  case "w": var audiow= new Audio("crash.mp3");
         audiow.play ();
   break;
 
-  case "a" : var audioa= new Audio("sounds/kick-bass.mp3");
+  case "a" : var audioa= new Audio("kick-bass.mp3");
         audioa.play ();
   break;
 
-  case "s": var audios= new Audio("sounds/snare.mp3");
+  case "s": var audios= new Audio("snare.mp3");
         audios.play ();
   break;
 
-  case "d": var audiod= new Audio("sounds/tom-1.mp3");
+  case "d": var audiod= new Audio("tom-1.mp3");
         audiod.play ();
   break;
 
-  case "j": var audioj= new Audio("sounds/tom-2.mp3");
+  case "j": var audioj= new Audio("tom-2.mp3");
         audioj.play ();
   break;
 
-  case "k": var audiok= new Audio("sounds/tom-3.mp3");
+  case "k": var audiok= new Audio("tom-3.mp3");
         audiok.play ();
   break;
 
-  case "l": var audiol= new Audio("sounds/tom-4.mp3");
+  case "l": var audiol= new Audio("tom-4.mp3");
         audiol.play ();
   break;
 
